348eab6d4ca7769cd0dd4927e72fb86346af983c
GM TEMP <graham.markall@embecosm.com>
2018-08-04 01:10:23 +0100

    Add communication logic. Add parsing function (untested)

--- lockbox.cpp
+++ lockbox.cpp
@@ -9,6 +9,37 @@ const int buttonPin = 6;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+uint32_t datahex(uint8_t* string, size_t slength) {
+
+    // must be 8 chars, null pointers bad news.
+    if(string == NULL || slength != 8) 
+       return NULL;
+
+    uint32_t res = 0xdeadbeef;
+
+    size_t index = 0;
+    while (index < slength) {
+        char c = string[index];
+        int value = 0;
+        if(c >= '0' && c <= '9')
+          value = (c - '0');
+        else if (c >= 'A' && c <= 'F') 
+          value = (10 + (c - 'A'));
+        else if (c >= 'a' && c <= 'f')
+          value = (10 + (c - 'a'));
+        else {
+          // non-hex digit
+          return NULL;
+        }
+
+        res = res & (value << ((7 - index) * 8));
+
+        index++;
+    }
+
+    return data;
+}
+
 extern "C"
 void setup() {
   
@@ -23,22 +54,67 @@ void setup() {
 
 int c = 0;
 uint32_t K = 0;
+bool locked = true;
+
+bool correctInput(uint8_t* recvBuf __attribute__((unused)))
+{
+  return true;
+}
+
+void doSerial() {
+  //if (c % 100 == 0) {
+  //  Serial.println(K, HEX);
+  //}
+
+  uint8_t recvBuf[9]; // Eight key chars plus terminator. Should be enough for anybody.
+  uint32_t bufLoc = 0; // Where are we writing to in the buffer?
+
+  Serial.print("Enter code: ");
+  while (true) {
+    if (Serial.available()) {
+      recvBuf[bufLoc] = Serial.read();
+      if (recvBuf[bufLoc] == '\n') {
+        Serial.write(recvBuf, bufLoc);
+        Serial.write("\n");
+        bufLoc = 0;
+        if (correctInput(recvBuf)) {
+          locked = false;
+          Serial.print("Unlocking...\n");
+          lcd.setCursor(0, 0);
+          lcd.print("UNLOCKED");
+        } else {
+          Serial.print("Enter code: ");
+        }
+      } else {
+        bufLoc++;
+      }
+    }
+  }
+}
 
 extern "C"
 void loop() {
-  unsigned int state = digitalRead(buttonPin);
-  lcd.setCursor(0, 1);
-  lcd.print("K: ");
-  if (state == LOW) {
-    randomSeed(millis());
-    K = random();
-    lcd.print(K);
-  }
+  lcd.setCursor(0, 0);
+  if (K) {
+    if (locked)
+      lcd.print("LOCKED          ");
+    else
+      lcd.print("UNLOCKED        ");
 
-  if (c % 100 == 0) {
-    Serial.println(K);
+
+    doSerial();
+  } else {
+    unsigned int state = digitalRead(buttonPin);
+    lcd.setCursor(0, 1);
+    lcd.print("K: ");
+    if (state == LOW) {
+      randomSeed(millis());
+      K = random();
+      lcd.print(K, HEX);
+    }
   }
 
   c += 1;
   delay(1);
 }
+
